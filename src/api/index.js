b2c45121b0c1f8e3c191a10fa1fdc8c696b58f43
mahadi-nsu <mahadi.hassan@northsouth.edu>
2020-04-25 01:24:22 +0600

    even better approach of destrcuturing and modify our api

--- src/api/index.js
+++ src/api/index.js
@@ -4,22 +4,21 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try {
-        // const { data } = await axios.get(url);
-        // return response.data;
-        // const modifiedData = {
-        //     confirmed: data.confirmed,
-        //     recovered: data.recovered,
-        //     deaths: data.death,
-        //     lastUpdate: data.lastUpdate,
-        // }
-
-        const response = await axios.get(url);
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
         const modifiedData = {
-            confirmed: response.data.confirmed,
-            recovered: response.data.recovered,
-            deaths: response.data.deaths,
-            lastUpdate: response.data.lastUpdate
+            confirmed: confirmed,
+            recovered: recovered,
+            deaths: deaths,
+            lastUpdate: lastUpdate,
         }
+
+        // const response = await axios.get(url);
+        // const modifiedData = {
+        //     confirmed: response.data.confirmed,
+        //     recovered: response.data.recovered,
+        //     deaths: response.data.deaths,
+        //     lastUpdate: response.data.lastUpdate
+        // }
         return modifiedData;
     } catch (eror) {
 
