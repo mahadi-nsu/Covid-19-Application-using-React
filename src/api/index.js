278bcabf1724a7581b2e4e00a1728f42379f25db
mahadi-nsu <mahadi.hassan@northsouth.edu>
2020-04-25 01:16:29 +0600

    api data modify without destrcturing

--- src/api/index.js
+++ src/api/index.js
@@ -4,15 +4,22 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try {
-        const { data } = await axios.get(url);
+        // const { data } = await axios.get(url);
         // return response.data;
-        const modifiedData = {
-            confirmed: data.confirmed,
-            recovered: data.recovered,
-            deaths: data.death,
-            lastUpdate: data.lastUpdate,
-        }
+        // const modifiedData = {
+        //     confirmed: data.confirmed,
+        //     recovered: data.recovered,
+        //     deaths: data.death,
+        //     lastUpdate: data.lastUpdate,
+        // }
 
+        const response = await axios.get(url);
+        const modifiedData = {
+            confirmed: response.data.confirmed,
+            recovered: response.data.recovered,
+            deaths: response.data.deaths,
+            lastUpdate: response.data.lastUpdate
+        }
         return modifiedData;
     } catch (eror) {
 
