187e07392a4ca38dc85b0bc8588a667c7345d137
mahadi-nsu <mahadi.hassan@northsouth.edu>
2020-04-25 01:06:19 +0600

    modify api data according tpo our need

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Cards from './components/Cards/Cards';
 import CountryPicker from './components/CountryPicker/CountryPicker';
 import Chart from './components/Chart/Chart';
+import { fetchData } from './api';
 
-function App() {
-  return (
-    <div>
-      <Cards />
-      <CountryPicker />
-      <Chart />
-    </div>
-  );
+import styles from './App.module.css';
+
+class App extends Component {
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    )
+  }
 }
 
 export default App;
